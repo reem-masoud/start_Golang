adaacc9544137ec242c35d89b0db96f2c39a290a
reem-masoud <65132189+reem-masoud@users.noreply.github.com>
2020-05-11 22:19:02 +0200

    Add files via upload

---
+++ a1.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+fmt.Println("hello world")
+const c=5
+fmt.Println("c is",5)
+
+var x int =2
+var y float32=2.4
+var z int=x+int(y)
+fmt.Println(z)
+
+}
